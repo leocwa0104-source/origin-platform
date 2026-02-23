e431da2936c71a5595d62118b78379706596124a
garfline <leocwa0104@gmail.com>
2026-02-23 21:40:44 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,6 @@ import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import Profile from './pages/Profile'
-import CreateContent from './pages/CreateContent'
-import ContentDetail from './pages/ContentDetail'
-import CopyrightCert from './pages/CopyrightCert'
-import CopyrightProtection from './pages/CopyrightProtection'
-import Monetization from './pages/Monetization'
-import DAO from './pages/DAO'
-import Admin from './pages/Admin'
 
 // 导入全局上下文
 import { AuthProvider } from './context/AuthContext'
@@ -33,8 +24,6 @@ function App() {
               <div className="logo">Origin原创者平台</div>
               <div className="header-menu">
                 <a href="/">首页</a>
-                <a href="/create">创作</a>
-                <a href="/dashboard">仪表盘</a>
                 <a href="/login">登录</a>
                 <a href="/register">注册</a>
               </div>
@@ -44,15 +33,6 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/profile/:userId" element={<Profile />} />
-                <Route path="/create" element={<CreateContent />} />
-                <Route path="/content/:contentId" element={<ContentDetail />} />
-                <Route path="/copyright/:contentId" element={<CopyrightCert />} />
-                <Route path="/copyright-protection" element={<CopyrightProtection />} />
-                <Route path="/monetization" element={<Monetization />} />
-                <Route path="/dao" element={<DAO />} />
-                <Route path="/admin" element={<Admin />} />
               </Routes>
             </Content>
             <Footer className="app-footer">
